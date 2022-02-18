aa668c31a417c0c544bf7b765a3a730b93f4530d
echedey-luis-alvarez <80125792+echedey-ls@users.noreply.github.com>
2022-02-18 12:33:26 +0100

    Un poco de refactoring
    No he solucionado el desfase de hora, eso lo haremos con: https://github.com/PaulStoffregen/DS1307RTC/blob/master/examples/SetTime/SetTime.ino

--- horarios_prohibidos/horarios_prohibidos.ino
+++ horarios_prohibidos/horarios_prohibidos.ino
@@ -1,118 +1,112 @@
 //Horarios prohibidos by Marce by CREA by tuviejosabroso
-#include <Wire.h>
-#include <TimeLib.h>
-#include <DS1307RTC.h>
 #include <LiquidCrystal.h>
 #include <TimeAlarms.h>
+#include <DS1307RTC.h>
 
 #define COLS 16
 #define ROWS 2
 
-LiquidCrystal lcd(12,11,5,4,3,2);
-tmElements_t datetime;
-char DiaSemana[][4] = {"Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab" };
+static LiquidCrystal lcd(12,11,5,4,3,2);
+static tmElements_t tm;
 
 void setup() {
   pinMode(8, OUTPUT); 
   Serial.begin(9600);
-  lcd.begin(COLS,ROWS);
+  lcd.begin(COLS, ROWS);
   setSyncProvider(RTC.get);
   if (timeStatus() != timeSet)
     lcd.print("Fallo de RTC");
   else
     lcd.print("Sincronizado con RTC");
-  EventoHDB;
-  //Programar horarios prohibidos
-  //Recuerda ponerlos desfasados -4 min
-  Alarm.alarmRepeat(dowTuesday, 17, 26, 0,EventoHP);
-  Alarm.alarmRepeat(dowTuesday, 19, 26, 0,EventoHDB);
-  Alarm.alarmRepeat(dowThursday, 11, 26, 0,EventoHP);
-  Alarm.alarmRepeat(dowThursday, 13, 26, 0,EventoHDB);
-  Alarm.alarmRepeat(21, 26, 0, EventoHP);
-  Alarm.alarmRepeat(8, 26, 0, EventoHDB);
+  
+  // Habría que hacer un check para tener claro que no estamos en horarios malos
+  // Pero el error es despreciable, ya que implica que haya alguien que lo sepa en el momento
+  // Y solo pasa una vez, o cuando se reconecta la alimentación
+  horasLibertad();
+  
+  // Programar horarios prohibidos
+  //Alarmas semanales
+  Alarm.alarmRepeat(dowTuesday , 17, 25, 0, horasSilencio);
+  Alarm.alarmRepeat(dowTuesday , 19, 35, 0, horasLibertad);
+  Alarm.alarmRepeat(dowThursday, 11, 25, 0, horasSilencio);
+  Alarm.alarmRepeat(dowThursday, 13, 35, 0, horasLibertad);
+  //Alarmas diarias (nocturna/diurna, ¿es necesario? I'll never know)
+  Alarm.alarmRepeat(21, 30, 0, horasSilencio);
+  Alarm.alarmRepeat( 8, 30, 0, horasLibertad);
 }
 
 void loop() {
-  imprimir_fecha();
-}
-/**
-   Funcion auxiliar para imprimir siempre 2 digitos
-*/
-void print2digits(int number) {
-  if (number >= 0 && number < 10) {
-    lcd.print('0');
-  }
-  lcd.print(number);
-}
-void imprimir_fecha(){
-  // Declaramos una estructura "tm" llamada datetime para almacenar
-  // La fecha y la hora actual obtenida desde el chip RTC.
   // Obtener la fecha y hora desde el chip RTC
-  if (RTC.read(datetime)) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Time: ");
-    print2digits(datetime.Hour);
-    lcd.write(':');
-    print2digits(datetime.Minute+4);
-    lcd.write(':');
-    print2digits(datetime.Second);
-    lcd.setCursor(0, 1);
-    lcd.print("Date: ");
-    lcd.print(datetime.Day);
-    lcd.write('/');
-    lcd.print(datetime.Month);
-    lcd.write('/');
-    lcd.print(tmYearToCalendar(datetime.Year));
+  if (RTC.read(tm)) {
+    imprimir_fecha_hora();
   }
   else {
     if (RTC.chipPresent()) {
       // El reloj esta detenido, es necesario ponerlo a tiempo
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("EL DS1307 ESTA DETENIDO");
+      lcd.print("DS1307 DETENIDO");
       lcd.setCursor(0, 1);
-      lcd.print("CORRE EL PROGRAMA PARA PONERLO A TIEMPO");
+      lcd.print("EJECUTE PROGRAMA");
     }
     else {
       // No se puede comunicar con el RTC en el bus I2C, revisar las conexiones.
       lcd.clear();
-      lcd.print("NO SE DETECTO EL DS1307");
-      lcd.print("REVISA TUS CONEXIONES E INTENTA DE NUEVO");
+      lcd.print("  DS1307 NO SE");
+      lcd.setCursor(0, 1);
+      lcd.print(" PUDO DETECTAR");
     }
-  };
-  Alarm.delay(1000);
+  }
 }
+
+/**
+   Imprime la fecha desde la variable global "tm"
+*/
+void imprimir_fecha_hora(){
+  // Hora en la fila superior
+  lcd.setCursor(0, 0);
+  print2digits(tm.Hour);
+  lcd.write(':');
+  print2digits(tm.Minute);
+  lcd.write(':');
+  print2digits(tm.Second);
+  // Fecha en la fila inferior
+  lcd.setCursor(0, 1);
+  print2digits(tm.Day);
+  lcd.write('/');
+  print2digits(tm.Month);
+  lcd.write('/');
+  lcd.print(tmYearToCalendar(tm.Year));
+}
+
 /**
    Funcion callback que activa el relevador en el pin 3 (horario prohibido)
 */
-void EventoHP()
+void horasSilencio()
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Horas del mal");
+  lcd.setCursor(0, 10);
+  lcd.print("SHHH!");
   digitalWrite(8, HIGH);
 }
  
 /**
    Funcion callback que desactiva el relevador en el pin 3 (horario de bien)
 */
-void EventoHDB()
+void horasLibertad()
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Horas de bien");
+  lcd.setCursor(0, 11);
+  lcd.print("Yay!");
   digitalWrite(8, LOW);
 }
-void fiesta(){
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Partytime!");
-  digitalWrite(8, LOW);
-  for(int i=0;i<30; i++){
-    digitalWrite(8, LOW);
-    delay(100);
-    digitalWrite(8, HIGH);
-    delay(100);
-  };
+
+/**
+   Funcion auxiliar para imprimir siempre 2 digitos
+*/
+void print2digits(unsigned char number) {
+  if (number < 10) {
+    lcd.print('0');
+  }
+  lcd.print(number);
 }
